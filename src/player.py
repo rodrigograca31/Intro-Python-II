7e34abb9187dcc131e5cadab2645d21c16be98ee
Rodrigo Graça <rodrigograca31@gmail.com>
2020-04-08 16:33:41 +0100

    refactor: ♻️ rename player room property

--- src/player.py
+++ src/player.py
@@ -6,16 +6,16 @@ from colored import fg, bg, attr
 class Player:
     def __init__(self, name, room):
         self.name = name
-        self.room = room
+        self.current_room = room
 
     def __str__(self):
         return (
             "\n"
             + self.name
             + " is at: "
-            + self.room.name
+            + self.current_room.name
             + ", "
-            + self.room.description
+            + self.current_room.description
             + "\n"
         )
 
@@ -23,22 +23,22 @@ class Player:
         # self.room = room
 
         if direction == "n":
-            if self.room.n_to:
-                self.room = self.room.n_to
+            if self.current_room.n_to:
+                self.current_room = self.current_room.n_to
             else:
                 print("%sYou can't go in that direction %s" % (fg(1), attr(0)))
         elif direction == "s":
-            if self.room.s_to:
-                self.room = self.room.s_to
+            if self.current_room.s_to:
+                self.current_room = self.current_room.s_to
             else:
                 print("%sYou can't go in that direction %s" % (fg(1), attr(0)))
         elif direction == "w":
-            if self.room.w_to:
-                self.room = self.room.w_to
+            if self.current_room.w_to:
+                self.current_room = self.current_room.w_to
             else:
                 print("%sYou can't go in that direction %s" % (fg(1), attr(0)))
         elif direction == "e":
-            if self.room.e_to:
-                self.room = self.room.e_to
+            if self.current_room.e_to:
+                self.current_room = self.current_room.e_to
             else:
                 print("%sYou can't go in that direction %s" % (fg(1), attr(0)))
